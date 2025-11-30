308e1f91bea98f36f01356974da23c25fc75266a
diamondblock <62496819+dmblock@users.noreply.github.com>
2025-11-30 12:59:54 +0800

    Change language copy source from 'en' to 'zh-CN'

--- scripts/i18n.mjs
+++ scripts/i18n.mjs
@@ -5,7 +5,7 @@ const root = "./src/lang"
 const entry = "entry.ts"
 const langs = fs.readdirSync(root)
 langs
-  .filter((lang) => lang !== "en")
+  .filter((lang) => lang !== "zh-CN")
   .forEach((lang) => {
-    fs.copyFileSync(path.join(root, "en", entry), path.join(root, lang, entry))
+    fs.copyFileSync(path.join(root, "zh-CN", entry), path.join(root, lang, entry))
   })
